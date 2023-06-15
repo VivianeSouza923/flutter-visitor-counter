acd9323d933c1f609405166605f4286f97fb9cea
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-06-14 23:20:16 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,42 @@ void main() {
   runApp(const MyApp());
 }
 
+// widget responsável por todas as configurações do aplicativo
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        color: const Color.fromARGB(255, 215, 227, 233),
-        alignment: Alignment.center,
-        child: const Text("Olá, mundo!"),
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+// widget responsável pela página do aplicativo, a principal, no caso.
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text(
+            "Pode entrar!",
+            style: TextStyle(
+                fontSize: 30, color: Colors.white, fontWeight: FontWeight.w700),
+          ),
+          Text(
+            "0",
+            style: TextStyle(
+              fontSize: 100,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
