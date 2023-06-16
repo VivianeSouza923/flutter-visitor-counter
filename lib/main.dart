e2ec9053569919c0e469b0ec69734bb7a7be8f78
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-06-16 13:46:21 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -32,71 +33,81 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Pode entrar!",
-            style: TextStyle(
-                fontSize: 30, color: Colors.white, fontWeight: FontWeight.w700),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/cafeteria.jpg'),
+            fit: BoxFit.cover,
           ),
-          const Text(
-            "0",
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Pode entrar!",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700),
             ),
-          ),
+            const Text(
+              "0",
+              style: TextStyle(
+                fontSize: 100,
+                color: Colors.white,
+              ),
+            ),
 
-          //espaçamento entre o texto (o numero de pessoas) e os botões. Ou seja, o espaçamento vertical
-          const SizedBox(height: 54),
+            //espaçamento entre o texto (o numero de pessoas) e os botões. Ou seja, o espaçamento vertical
+            const SizedBox(height: 54),
 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  //padding: const EdgeInsets.all(32)
-                  fixedSize: const Size(100, 100),
-                  primary: Colors.black,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(70)),
-                ),
-                child: const Text(
-                  "Saiu",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    //padding: const EdgeInsets.all(32)
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(70)),
+                  ),
+                  child: const Text(
+                    "Saiu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
-              ),
 
-              // espaçamento entre os botões de saída e entrada
-              const SizedBox(width: 53),
+                // espaçamento entre os botões de saída e entrada
+                const SizedBox(width: 53),
 
-              TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  //padding: const EdgeInsets.all(32)
-                  fixedSize: const Size(100, 100),
-                  primary: Colors.black,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(70)),
-                ),
-                child: const Text(
-                  "Entrou",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
+                TextButton(
+                  onPressed: increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    //padding: const EdgeInsets.all(32)
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(70)),
+                  ),
+                  child: const Text(
+                    "Entrou",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
